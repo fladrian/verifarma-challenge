4a8bce61c0bc97a3f567e05564d455e4bbf55e3c
Adrián Flores <adriandev.fl@gmail.com>
2026-01-16 01:20:31 -0300

    fix: improving caching data for movies and movie detail, using one hour duration of staletime

--- src/data/composables/useMovies.ts
+++ src/data/composables/useMovies.ts
@@ -5,28 +5,33 @@ import type { Movie, MoviesResponse } from '@core/entities'
 
 const movieRepository = new MovieRepositoryImpl()
 
+const ONE_HOUR = 60 * 60 * 1000 // 1 hour
+
 
 export function useMovies(page: Ref<number>, year?: Ref<string>, type?: Ref<string>) {
   return useQuery<MoviesResponse>({
-    queryKey: ['movies', computed(() => page.value), computed(() => year?.value), computed(() => type?.value)],
+    queryKey: ['movies', page, year, type],
     queryFn: () => movieRepository.getMovies(page.value, year?.value, type?.value),
+    enabled: computed(() => page.value > 0),
+    staleTime: ONE_HOUR,
   })
 }
 
 export function useMovie(id: string | Ref<string> | ComputedRef<string>) {
   return useQuery<Movie>({
-    queryKey: ['movie', computed(() => unref(id))],
+    queryKey: ['movie', id],
     queryFn: () => movieRepository.getMovie(unref(id)),
     enabled: computed(() => {
       const value = unref(id)
       return !!value && value.length > 0
     }),
+    staleTime: ONE_HOUR,
   })
 }
 
 export function useFilterMovies(query: Ref<string>, page: Ref<number>, year?: Ref<string>, type?: Ref<string>) {
   return useQuery<MoviesResponse>({
-    queryKey: ['movies', 'search', computed(() => query.value), computed(() => page.value), computed(() => year?.value), computed(() => type?.value)],
+    queryKey: ['movies', 'search', query, page, year, type],
     queryFn: () => movieRepository.filterMovies(query.value, page.value, year?.value, type?.value),
     enabled: computed(() => query.value.length > 0),
   })
